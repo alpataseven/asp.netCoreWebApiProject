ef0d46f6737f36306d3d7fab85ec5eb9277ff04b
Alperen ATASEVEN <alperenataseven.3@gmail.com>
2025-07-10 19:21:51 +0300

    Crud işlemi tamamlandı

---
+++ ApiConsume/Hotelier.BusinessLayer/Concrete/TestimaonialManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hotelier.BusinessLayer.Abstract;
+using Hotelier.DataAccessLayer.Abstract;
+using Hotelier.EntityLayer.Concrete;
+
+namespace Hotelier.BusinessLayer.Concrete
+{
+    public class TestimaonialManager : ITestimonialService
+    {
+        private readonly ITestimonialDal _testimonialDal;
+        public TestimaonialManager(ITestimonialDal testimonialDal)
+        {
+            _testimonialDal = testimonialDal;
+        }
+        public void TDelete(Testimonial t)
+        {
+            _testimonialDal.Delete(t);
+        }
+
+        public Testimonial TGetByID(int id)
+        {
+            return _testimonialDal.GetByID(id);
+        }
+
+        public List<Testimonial> TGetList()
+        {
+            return _testimonialDal.GetList();
+        }
+
+        public void TInsert(Testimonial t)
+        {
+            _testimonialDal.Insert(t);
+        }
+
+        public void TUpdate(Testimonial t)
+        {
+            _testimonialDal.Update(t);
+        }
+    }
+}
